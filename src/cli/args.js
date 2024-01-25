168a5d64fecebc44bd1f1b2c8e594cd8046df935
Marigza <marigola@mail.ru>
2024-01-25 15:48:02 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const startIndex = process.argv.findIndex(arg=>arg.startsWith('--'))
+    const argsArray = process.argv.slice(startIndex);
+    const resultArray = [];
+    for (let i = 0; i < argsArray.length; i = i + 2) {
+        resultArray.push(`${argsArray[i]} is ${argsArray[i+1]}`)
+    }
+
+    console.log(resultArray.map(arg=>arg.slice(2)).join(', '))
 };
 
 parseArgs();
