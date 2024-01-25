50d7dfc3e7930450242cd0c57a64faf02ea761bc
Marigza <marigola@mail.ru>
2024-01-25 12:07:14 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const folderName = path.join(__dirname, '/files');
+
+    fs.readdir(folderName, (err, folderContent) => {
+        if (err) throw new Error('FS operation failed');
+        
+        console.log(folderContent);
+    })
 };
 
 await list();
