0272abe90f801ec4e7ad1b978f6dd4249632b39c
Marigza <marigola@mail.ru>
2024-01-25 14:55:21 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const processArray = Object.entries(process.env);
+    const filteredArray = processArray.filter(variable => variable[0].startsWith(prefix));
+    const result = filteredArray.map(env => `${env[0]}=${env[1]}`).join('; ');
+
+    if (result.length) {
+        console.log(result);
+    } else {
+        console.log('no such variables, you need to pass it when running script');
+    }
 };
 
 parseEnv();
