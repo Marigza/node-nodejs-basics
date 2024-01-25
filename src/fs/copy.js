13262c596742991bceb94856a53ab610cf926fa5
Marigza <marigola@mail.ru>
2024-01-25 10:51:36 +0300

    feat: implement copy.js function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
+import { mkdir, readdir, copyFile } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const directoryCreated = path.join(__dirname, '/files_copy');
+    const directoryForCopy = path.join(__dirname, '/files');
+
+    readdir(directoryForCopy, (err, dir) => {
+        if (err) throw new Error('FS operation failed');
+
+        mkdir(directoryCreated, (err) => {
+            if (err?.code === 'EEXIST') throw new Error('FS operation failed');
+        });
+
+        for (let file of dir) {
+            let fileScr = path.join(__dirname, `/files/${file}`);
+            let fileDest = path.join(directoryCreated, `/${file}`);
+            copyFile(fileScr, fileDest, err => {
+                if (err) throw new Error('FS operation failed');
+            })
+        }
+    })
+    
 };
 
 await copy();
