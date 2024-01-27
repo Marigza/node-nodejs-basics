a6d8a9520683a7f8d99ff01ecabb71d53a86e05c
Marigza <marigola@mail.ru>
2024-01-27 16:40:32 +0300

    feat: implement streams transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,11 @@
 const transform = async () => {
-    // Write your code here 
+    process.stdout.write('Please enter text and press ENTER\n');
+
+    process.stdin.on('data', (data)=> {
+        const reversedData = data.toString().split('').reverse().join('');
+        process.stdout.write(`reversed text: ${reversedData}`);
+        process.exit()
+    })
 };
 
 await transform();
